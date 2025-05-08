77eff58bee3dc14e127663f4412383cbad08f7d5
Noah Boos <nboosgamemaker@gmail.com>
2025-05-08 22:27:43 +0200

    Features - Added custom name and icon for HomeView

--- src/windows/HomeView.ts
+++ src/windows/HomeView.ts
@@ -21,7 +21,11 @@ export class HomeView extends ItemView {
     }
 
     getDisplayText(): string {
-        return "";
+        return "Habitudes";
+    }
+
+    getIcon(): string {
+        return "book";
     }
 
     async onOpen() {
