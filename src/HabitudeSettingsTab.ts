a2161f3ef10b397306c258bff7d146d5f473b4c8
Noah Boos <nboosgamemaker@gmail.com>
2025-05-08 21:52:23 +0200

    Features - Added username setting

--- src/HabitudeSettingsTab.ts
+++ src/HabitudeSettingsTab.ts
@@ -1,12 +1,12 @@
-import {App, PluginSettingTab} from "obsidian";
+import {App, PluginSettingTab, Setting} from "obsidian";
 import Habitude from "./main";
 
 export interface HabitudeSettings {
-
+    username: string;
 }
 
 export const DEFAULT_SETTINGS: HabitudeSettings = {
-
+    username: "Username",
 }
 
 export class HabitudeSettingsTab extends PluginSettingTab {
@@ -20,5 +20,16 @@ export class HabitudeSettingsTab extends PluginSettingTab {
     display(): void {
         const {containerEl} = this;
         containerEl.empty();
+
+        new Setting(containerEl)
+            .setName("Username")
+            .setDesc("The username that will be used in some contextual message.")
+            .addText(text => text
+                .setPlaceholder("Username")
+                .setValue(this.plugin.settings.username)
+                .onChange(async (value) => {
+                    this.plugin.settings.username = value;
+                    await this.plugin.saveSettings();
+                }));
     }
 }
